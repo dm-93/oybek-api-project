31626c2e0f5504986a1361f1ad233de4340b63f6
Denis Martynenko <denis.martynenko93@gmail.com>
2020-01-30 16:33:33 +0300

    Implement output of all users in All Users tab

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/shared/services/login.service';
+import { Router } from '@angular/router';
+import { UserManagementService } from 'src/app/shared/services/user-management.service';
+import { UsersListModel } from 'src/app/shared/models/users-list.model';
+import { UserModel } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-users-list',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersListComponent implements OnInit {
 
-  constructor() { }
+  usersList: Array < UserModel > = [];
 
+  constructor(
+    private loginService: LoginService,
+    private userManagService: UserManagementService,
+    private Router: Router
+  ) {}
   ngOnInit() {
+    this.userManagService.getUsers(1).subscribe(
+      response => {
+        this.usersList = response.Data
+        //console.log(this.usersList);
+      }
+    );
   }
 
 }
